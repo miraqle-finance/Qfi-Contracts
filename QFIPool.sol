bad350982ad6ed3c9a4c450db16b01869fce6065
miraqle-finance <81742720+miraqle-finance@users.noreply.github.com>
2021-04-01 17:04:30 +0900

    Add files via upload

---
+++ QFIPool.sol
@@ -0,0 +1,265 @@
+pragma solidity 0.6.12;
+
+
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./MiraqleDefi.sol";
+
+
+interface IMigratorPool {
+    function migrate(IERC20 token) external returns (IERC20);
+}
+
+
+contract QFIPool is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken;
+        uint256 allocPoint;
+        uint256 lastRewardBlock;
+        uint256 accTokenPerShare;
+    }
+
+    MiraqleDefi public token;
+    address public teamAddr;
+    uint256 public bonusEndBlock;
+    uint256 public tokenPerBlock;
+    uint256 public constant BONUS_MULTIPLIER = 2;
+    IMigratorPool public migrator;
+    PoolInfo[] public poolInfo;
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    uint256 public totalAllocPoint;
+    uint256 public startBlock;
+
+    uint256 public endBlock;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(
+        MiraqleDefi _token,
+        address _teamAddr,
+        uint256 _tokenPerBlock,
+        uint256 _startBlock,
+        uint256 _endBlock,
+        uint256 _bonusEndBlock
+    ) public {
+        token = _token;
+        teamAddr = _teamAddr;
+        tokenPerBlock = _tokenPerBlock;
+        startBlock = _startBlock;
+        endBlock = _endBlock;
+        bonusEndBlock = _bonusEndBlock;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: lastRewardBlock,
+            accTokenPerShare: 0
+        }));
+    }
+
+    // Update the given pool's Token allocation point. Can only be called by the owner.
+    function set(uint256 _pid, uint256 _allocPoint, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    // Set the migrator contract. Can only be called by the owner.
+    function setMigrator(IMigratorPool _migrator) public onlyOwner {
+        migrator = _migrator;
+    }
+
+    // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
+    function migrate(uint256 _pid) public {
+        require(address(migrator) != address(0), "migrate: no migrator");
+        PoolInfo storage pool = poolInfo[_pid];
+        IERC20 lpToken = pool.lpToken;
+        uint256 bal = lpToken.balanceOf(address(this));
+        lpToken.safeApprove(address(migrator), bal);
+        IERC20 newLpToken = migrator.migrate(lpToken);
+        require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
+        pool.lpToken = newLpToken;
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        if (_to <= bonusEndBlock) {
+            return _to.sub(_from).mul(BONUS_MULTIPLIER);
+        } else if (_from >= bonusEndBlock) {
+            return _to.sub(_from);
+        } else {
+            return bonusEndBlock.sub(_from).mul(BONUS_MULTIPLIER).add(
+                _to.sub(bonusEndBlock)
+            );
+        }
+    }
+
+    // View function to see pending Tokens on frontend.
+    function pendingToken(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accTokenPerShare = pool.accTokenPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+
+        uint256 blockNumber = 0;
+        if(block.number > endBlock)
+            blockNumber = endBlock;
+        else
+            blockNumber = block.number;
+        
+        if (blockNumber > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, blockNumber);
+            uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accTokenPerShare = accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accTokenPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Update reward vairables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            if (block.number > endBlock)
+                pool.lastRewardBlock = endBlock;
+            else
+                pool.lastRewardBlock = block.number;
+            return;
+        }
+        if (pool.lastRewardBlock == endBlock){
+            return;
+        }
+
+        if (block.number > endBlock){
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, endBlock);
+            uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            //token.mint(teamAddr, tokenReward.div(10));
+            //token.mint(address(this), tokenReward);
+            pool.accTokenPerShare = pool.accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
+            pool.lastRewardBlock = endBlock;
+            return;
+        }
+
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        //token.mint(teamAddr, tokenReward.div(10));
+        //token.mint(address(this), tokenReward);
+        pool.accTokenPerShare = pool.accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+        
+    }
+
+    // Deposit LP tokens to Pool for Token allocation.
+    function deposit(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accTokenPerShare).div(1e12).sub(user.rewardDebt);
+            safeTokenTransfer(msg.sender, pending);
+        }
+        if(_amount > 0) { //kevin
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+    
+    // DepositFor LP tokens to Pool for Token allocation.
+    function depositFor(address _beneficiary, uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_beneficiary];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accTokenPerShare).div(1e12).sub(user.rewardDebt);
+            safeTokenTransfer(_beneficiary, pending);
+        }
+        if(_amount > 0) { //kevin
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(1e12);
+        emit Deposit(_beneficiary, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from Pool.
+    function withdraw(uint256 _pid, uint256 _amount) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "withdraw: not good");
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accTokenPerShare).div(1e12).sub(user.rewardDebt);
+        safeTokenTransfer(msg.sender, pending);
+        user.amount = user.amount.sub(_amount);
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(1e12);
+        pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    // Safe token transfer function, just in case if rounding error causes pool to not have enough Tokens.
+    function safeTokenTransfer(address _to, uint256 _amount) internal {
+        uint256 tokenBal = token.balanceOf(address(this));
+        if (_amount > tokenBal) {
+            token.transfer(_to, tokenBal);
+        } else {
+            token.transfer(_to, _amount);
+        }
+    }
+
+    // Update team address by the previous team.
+    function team(address _teamAddr) public {
+        require(msg.sender == teamAddr, "dev: wut?");
+        teamAddr = _teamAddr;
+    }
+
+}
